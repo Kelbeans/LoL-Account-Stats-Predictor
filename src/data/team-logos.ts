3414d0d1034af05ab18711a57957fc5044d69725
kelvin-malabanan-rp <kelvin.malabanan@rocketpartners.io>
2026-06-18 16:52:57 +0800

    fix: use local SVG team logos instead of broken CDN URLs

--- src/data/team-logos.ts
+++ src/data/team-logos.ts
@@ -1,13 +1,13 @@
 export const TEAM_LOGOS: Record<string, string> = {
-  T1: 'https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1631819614066_t1-t1_2021.png',
-  TLAW: 'https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1631819456274_tl-tl.png',
-  KC: 'https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FKarmine-Corp-Logo2.png',
-  DCG: 'https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FDCG-logo.png',
+  T1: '/logos/t1.svg',
+  TLAW: '/logos/tlaw.svg',
+  KC: '/logos/kc.svg',
+  DCG: '/logos/dcg.svg',
 };
 
 export const TEAM_COLORS: Record<string, string> = {
   T1: '#e2012d',
-  TLAW: '#0a2f5c',
-  KC: '#00a3e0',
-  DCG: '#1a5276',
+  TLAW: '#0c2340',
+  KC: '#00b4d8',
+  DCG: '#1b3a5c',
 };
